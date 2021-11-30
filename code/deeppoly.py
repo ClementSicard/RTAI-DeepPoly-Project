e54414e64d07ced53a9e04b3fd5afb09a835341f
Clément Sicard <clement.sicard@icloud.com>
2021-11-30 22:27:11 +0100

    added forward for SPU transformer

--- code/deeppoly.py
+++ code/deeppoly.py
@@ -1,5 +1,6 @@
 from math import ceil
 import torch
+from torch import overrides
 from networks import SPU, Normalization, DerivativeSPU
 from typing import List, Tuple
 
@@ -7,7 +8,7 @@ from typing import List, Tuple
 class DeepPolyCertifier:
     net: torch.nn.Module
     eps: torch.float
-    inputs: torch.FloatTensor
+    inputs: torch.Tensor
     true_label: int
     transformers: List[torch.nn.Module]
     verifier: torch.nn.Module
@@ -16,7 +17,7 @@ class DeepPolyCertifier:
         self,
         net: torch.nn.Module,
         eps: float,
-        inputs: torch.FloatTensor,
+        inputs: torch.Tensor,
         true_label: int
     ) -> None:
         self.net = net
@@ -65,17 +66,17 @@ class DeepPolyCertifier:
 
 class DeepPolyInputsTransformer(torch.nn.Module):
     eps: torch.float
-    bounds: torch.FloatTensor
+    bounds: torch.Tensor
 
     def __init__(self, eps: torch.float) -> None:
         super(DeepPolyInputsTransformer, self).__init__()
         self.eps = eps
 
-    def forward(self, inputs: torch.FloatTensor):
+    def forward(self, inputs: torch.Tensor):
         self.bounds = inputs.repeat(2, 1, 1, 1)
 
         # Add epsilon around the input bounds
-        self.bounds += torch.FloatTensor([[[[-self.eps]]], [[[self.eps]]]])
+        self.bounds += torch.Tensor([[[[-self.eps]]], [[[self.eps]]]])
 
         # Clamp them back to intersect with [0, 1]^n
         self.bounds = torch.clamp(self.bounds, 0, 1)
@@ -85,31 +86,31 @@ class DeepPolyInputsTransformer(torch.nn.Module):
 
 class DeepPolyNormalizationTransformer(torch.nn.Module):
     previous: torch.nn.Module
-    std: torch.FloatTensor
-    mean: torch.FloatTensor
-    bounds: torch.FloatTensor
+    std: torch.Tensor
+    mean: torch.Tensor
+    bounds: torch.Tensor
 
     def __init__(self, previous: torch.nn.Module) -> None:
         super(DeepPolyNormalizationTransformer, self).__init__()
         self.previous = previous
 
-        self.std = torch.FloatTensor([0.3081])
-        self.mean = torch.FloatTensor([0.1307])
+        self.std = torch.Tensor([0.3081])
+        self.mean = torch.Tensor([0.1307])
 
-    def forward(self, bounds: torch.FloatTensor):
+    def forward(self, bounds: torch.Tensor):
         self.bounds = torch.div(bounds - self.mean, self.std)
         return self.bounds
 
 
 class DeepPolyFlattenTransformer(torch.nn.Module):
     previous: torch.nn.Module
-    bounds: torch.FloatTensor
+    bounds: torch.Tensor
 
     def __init__(self, previous: torch.nn.Module) -> None:
         super(DeepPolyFlattenTransformer, self).__init__()
         self.previous = previous
 
-    def forward(self, bounds: torch.FloatTensor):
+    def forward(self, bounds: torch.Tensor):
         return torch.stack(
             tensors=[
                 bounds[0, :, :, :].flatten(),
@@ -118,7 +119,7 @@ class DeepPolyFlattenTransformer(torch.nn.Module):
             dim=1
         )
 
-    def _back_substitution_helper(self, max_steps: int, parameters: Tuple[torch.FloatTensor, ...] = None):
+    def _back_substitution_helper(self, max_steps: int, parameters: Tuple[torch.Tensor, ...] = None):
         self.bounds = self.last._back_substitution(
             max_steps=max_steps, parameters=parameters)
         slicing_idx = int(len(self.bounds) / 2.0)
@@ -140,7 +141,7 @@ class DeepPolyAffineTransformer(torch.nn.Module):
     weights: torch.Tensor
     w_max: torch.Tensor
     w_min: torch.Tensor
-    bounds: torch.FloatTensor
+    bounds: torch.Tensor
     bs_steps: int
 
     def __init__(self, bias: torch.Tensor = None, weights: torch.Tensor = None, previous: torch.nn.Module = None, bs_steps: int = 0) -> None:
@@ -157,7 +158,7 @@ class DeepPolyAffineTransformer(torch.nn.Module):
         # Backsubsitution steps
         self.bs_steps = bs_steps
 
-    def _back_sub_helper(self, max_steps: int, parameters: Tuple[torch.FloatTensor, ...] = None):
+    def _back_sub_helper(self, max_steps: int, parameters: Tuple[torch.Tensor, ...] = None):
         if parameters:
             assert(len(parameters) == 4)
             w_l, w_u, b_l, b_u = parameters
@@ -202,7 +203,7 @@ class DeepPolyAffineTransformer(torch.nn.Module):
         self.bounds[upper_bound_indices,
                     1] = updated_bounds[upper_bound_indices, 1]
 
-    def forward(self, bounds: torch.FloatTensor) -> torch.FloatTensor:
+    def forward(self, bounds: torch.Tensor) -> torch.Tensor:
         l_b = torch.matmul(
             self.w_max, bounds[:, 0]) + torch.matmul(self.w_min, bounds[:, 1])
         u_b = torch.matmul(
@@ -219,14 +220,159 @@ class DeepPolyAffineTransformer(torch.nn.Module):
 
 class DeepPolySPUTransformer(torch.nn.Module):
     previous: torch.nn.Module
+    derivative_SPU: torch.nn.Module
+    compute_SPU: torch.nn.Module
+    bounds: torch.Tensor
+    zero_SPU: torch.Tensor
 
     def __init__(self, previous: torch.nn.Module = None) -> None:
         super(DeepPolySPUTransformer, self).__init__()
-
         self.previous = previous
 
-    def forward(self, bounds: torch.FloatTensor):
-        pass
+        self.derivative_spu = DerivativeSPU()
+        self.compute_SPU = SPU()
+        self.zero_SPU = torch.sqrt(torch.abs(SPU(torch.tensor(0))))
+
+    def forward(self, bounds: torch.Tensor):
+        return self._compute_linear_bounds(bounds)
 
     def _back_substitution_helper(self):
         pass
+
+    def _compute_linear_bounds(self, bounds: torch.Tensor):
+        self.bounds = bounds
+
+        """
+        Generate indices for the 5 cases
+        """
+
+        # 1st case: upper <= 0, lower <= 0
+        idx_1 = self.bounds[:, 1] <= 0
+
+        # 2nd case: lower >= 0
+        idx_2 = self.bounds[:, 0] >= 0
+
+        # 3rd case: upper >= zero_SPU
+        idx_3 = self.bounds[:, 1] >= self.zero_SPU
+
+        # all the remaining indices
+        tmp_idx_4 = torch.logical_and(
+            torch.logical_and(
+                torch.logical_not(idx_1),
+                torch.logical_not(idx_2)
+            ),
+            torch.logical_not(idx_3),
+        )
+
+        # 4th case: the other cases that verify with SPU(l) > SPU(u)
+        idx_4 = torch.logical_and(
+            tmp_idx_4,
+            self.compute_SPU(self.bounds[:, 0]) > self.compute_SPU(
+                self.bounds[:, 1])
+        )
+
+        # 5th case: all the other cases
+        idx_5 = torch.logical_and(tmp_idx_4, torch.logical_not(idx_4))
+
+        # Initiate the weights and bias for all the inputs
+        self.bound_weights = torch.zeros(self.bounds.shape[0], 2)
+        self.bound_bias = torch.zeros(self.bounds.shape[0], 2)
+
+        """
+        1st case: u <= 0
+        """
+        # w_l = (self.compute_SPU(u) - self.compute_SPU(l)) / (u - l)
+        self.bound_weights[idx_1, 0] = (self.compute_SPU(self.bounds[idx_1, 1]) - self.compute_SPU(
+            self.bounds[idx_1, 0])) / (self.bounds[idx_1, 1] - self.bounds[idx_1, 0])
+
+        # b_l = SPU(l) - w_l * l
+        self.bound_bias[idx_1, 0] = self.compute_SPU(
+            self.bounds[idx_1, 0]) - self.bound_weights[idx_1, 0] * self.bounds[idx_1, 0]
+
+        # w_u = 0 (already the case as initiated)
+
+        # b_u = SPU(l)
+        self.bound_bias[idx_1, 1] = self.compute_SPU(self.bounds[idx_1, 0])
+
+        """
+        2nd case: l >= 0
+        """
+        # Optimal point(s)
+        # a = (u + l) / 2
+        optimal_points_2 = (self.bounds[idx_2, 1] + self.bounds[idx_2, 0]) / 2
+
+        # w_l = self.derivative_SPU(a)
+        self.bound_weights[idx_2, 0] = self.derivative_SPU(optimal_points_2)
+
+        # b_l = self.compute_SPU(a) - a * self.derivative_SPU(a)
+        self.bound_bias[idx_2, 0] = self.compute_SPU(
+            optimal_points_2) - optimal_points_2 * self.derivative_SPU(optimal_points_2)
+
+        # w_u = u + l
+        self.bound_weights[idx_2, 1] = self.bounds[idx_2,
+                                                   1] + self.bounds[idx_2, 0]
+        # b_u = -u * l - self.compute_SPU(0)
+        self.bound_bias[idx_2, 1] = -self.bounds[idx_2, 1] * \
+            self.bounds[idx_2, 0] - self.compute_SPU(0)
+
+        """
+        3rd case: u >= SPU^(-1)(0)
+        """
+        # w_u = (self.compute_SPU(u) - self.compute_SPU(l)) / (u - l)
+        self.bound_weights[idx_3, 1] = (self.compute_SPU(self.bounds[idx_3, 1]) - self.compute_SPU(
+            self.bounds[idx_3, 0])) / (self.bounds[idx_3, 1] - self.bounds[idx_3, 0])
+
+        # b_u = self.compute_SPU(l) - w_u * l
+        self.bound_bias[idx_3, 1] = self.compute_SPU(
+            self.bounds[idx_3, 0]) - self.bound_weights[idx_3, 1] * self.bounds[idx_3, 0]
+
+        # Optimal point
+        # a = max((u + l) / 2, SPU_equals_to_zero)
+        optimal_points_3 = torch.max(
+            (self.bounds[idx_3, 1] + self.bounds[idx_3, 0]) / 2, self.zero_SPU)
+
+        # w_l = self.derivative_SPU(a)
+        self.bound_weights[idx_3, 0] = self.derivative_SPU(optimal_points_3)
+        # b_l = self.compute_SPU(a) - a * self.derivative_SPU(a)
+        self.bound_bias[idx_3, 0] = self.compute_SPU(
+            optimal_points_3) - optimal_points_3 * self.derivative_SPU(optimal_points_3)
+
+        """
+        4th case: all the other indices that verify SPU(l) > SPU(u)
+        """
+
+        # w_u = 0 (already the case)
+
+        # b_u = max(self.compute_SPU(l), self.compute_SPU(u))
+        self.bound_bias[idx_4, 1] = torch.max(
+            self.compute_SPU(self.bounds[idx_4, 0]), self.compute_SPU(self.bounds[idx_4, 1]))
+
+        # w_l = 0.0 (already the case)
+
+        # b_l = self.compute_SPU(0)
+        self.bound_bias[idx_4, 0] = self.compute_SPU(0)
+
+        """
+        5th case: all the other indices
+        """
+
+        # w_u = (self.compute_SPU(u) - self.compute_SPU(l)) / (u - l)
+        self.bound_weights[idx_5, 1] = (self.compute_SPU(self.bounds[idx_5, 1]) - self.compute_SPU(
+            self.bounds[idx_5, 0])) / (self.bounds[idx_5, 1] - self.bounds[idx_5, 0])
+        # b_u = self.compute_SPU(l) - w_u * l
+        self.bound_bias[idx_5, 1] = self.compute_SPU(
+            self.bounds[idx_5, 0]) - self.bound_weights[idx_5, 1] * self.bounds[idx_5, 0]
+
+        # w_l = 0.0 (already the case)
+
+        # b_l = self.compute_SPU(0)
+        self.bound_bias[idx_5, 0] = self.compute_SPU(0)
+
+        """
+        Done generating weights and bias for all the indices.
+
+        Compute bound transformation
+        """
+        self.bounds = self.bound_weights * self.bounds + self.bound_bias
+
+        return self.bounds
